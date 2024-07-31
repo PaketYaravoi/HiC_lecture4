59e1d4994b8995264e16dd2f14a2e97cf38d57e2
PaketYaravoi <roma.klimov10.94@gmail.com>
2024-07-31 12:37:41 +1100

    1 task

--- inputTasks/Program.cs
+++ inputTasks/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] matrix = GetMatrix(8, 8);
+
+ShowMatrix(matrix);
+
+int[,] GetMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(0, 2);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
