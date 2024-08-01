1f62b49e33ff318ab3d854ee4a9986d8324d85db
PaketYaravoi <roma.klimov10.94@gmail.com>
2024-08-01 11:32:45 +1100

    create task4: work with strings

---
+++ Task4_String/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 4 напишите программу , которая принимает на воход строку с цифрами и латинскими буквами, а на выходе покажет строку только с латинскими символами:
+//    Например: "234Password63" -> "Password"
+
+string input = Console.ReadLine();
+Console.WriteLine(input);
+Console.WriteLine();
+
+string stringNew = GetLetterFromString(input);
+Console.WriteLine(stringNew);
+
+string GetLetterFromString(string str)
+{
+    string result = String.Empty;
+
+    foreach (char s in str)
+    {
+        if (char.IsAsciiLetter(s) == true)
+        {
+            result += s; // если компелятор увидит что к строке мы пытаемся прибавить(+) символ , ругаться он не будет , и првильно все склеит
+        }
+
+    }
+    return result;
+} 
