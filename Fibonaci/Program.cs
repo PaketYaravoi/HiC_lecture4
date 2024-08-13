0af03aa515a966ec50d37ff4913b489d44047c71
PaketYaravoi <roma.klimov10.94@gmail.com>
2024-08-13 11:36:25 +1100

    Создал рекурсивный метод в задаче Fibonaci

--- Fibonaci/Program.cs
+++ Fibonaci/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter integer: ");
+
+        int number = 0;
+        while (!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("Input Error!");
+        }
+        Console.WriteLine();
+
+        for (int i = 1; i <= number; i++)
+        {
+            Console.Write($"Fibonaci at number {i} =  ");
+            Console.WriteLine(Fibonachi(i));
+        }
+        
+        static double Fibonachi(int num)
+        {
+            if (num == 1 || num == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonachi(num - 1) + Fibonachi(num - 2);
+            
+            }
+
+
+        }
+    }
+}
